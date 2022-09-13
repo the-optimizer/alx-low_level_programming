b69ed6131ed79627d71af7a3bf6c474c26238971
the-optimizer <bn.ewang123@gmail.com>
2022-09-12 19:01:31 -0700

    add ficx

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  *  main - entry point
  *  Description: prints  if a number is positive or negative.
  *  Return: 0
  */
+
 int main(void)
 {
 	int n;
